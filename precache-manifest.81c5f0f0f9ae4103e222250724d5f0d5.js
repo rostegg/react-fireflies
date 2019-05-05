56dd6355290c64e026207e6a18abc80f9e24f1c9
rostegg <rostik.nikolaenko@gmail.com>
2019-05-05 23:42:01 +0300

    Updates

---
+++ precache-manifest.81c5f0f0f9ae4103e222250724d5f0d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13263859ead88ed8dc126162ae0ed870",
+    "url": "/react-fireflies/index.html"
+  },
+  {
+    "revision": "9ca422ab1918288c9351",
+    "url": "/react-fireflies/static/css/2.9b17ae97.chunk.css"
+  },
+  {
+    "revision": "c4d14d3ea1b148b43d6b",
+    "url": "/react-fireflies/static/css/main.4ed8c0ca.chunk.css"
+  },
+  {
+    "revision": "9ca422ab1918288c9351",
+    "url": "/react-fireflies/static/js/2.1deb380a.chunk.js"
+  },
+  {
+    "revision": "c4d14d3ea1b148b43d6b",
+    "url": "/react-fireflies/static/js/main.117cc66d.chunk.js"
+  },
+  {
+    "revision": "dbec19fb605250137424",
+    "url": "/react-fireflies/static/js/runtime~main.1227fc81.js"
+  }
+]);
